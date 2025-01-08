b1ffbc71d323283af9b015b4f215925e9179ac8f
Eshan_Abeysekara <87514961+Eshan-Abeysekara@users.noreply.github.com>
2025-01-08 17:20:29 +0530

    connect backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ShopContextProvider>
       <App />
